5466171b5c8d6d54dfbb859e4a5b15978c2a6cac
Charles Cruse <charle9@vt.edu>
2023-08-18 01:27:00 -0400

    exercise 06 solution updated

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -9,7 +9,57 @@ interface User {
    * - 'super-admin'
    */
   role: "user" | "admin" | "super-admin";
+  // role: Role;
 }
+
+// type Role = "user" | "admin" | "super-admin"
+
+
+
+
+//This is the same as just combining two objects into one, which will have all keys. By adding the union (|), then we are saying it can specifically become an object with 
+//the first object combined with one of the three second objects. Called a discriminative union because we descriminate on a key, in this case 'role'
+// type User = {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+// } & (
+//   |{
+//     role: "admin";
+//     adminPassword: string;
+//   } | { 
+//     role: "user";
+//   } | {
+//     role: "super-admin";
+//     superAdminPassword: string;
+//   }
+// )
+
+
+// We can also declare the discriminative object as a separate thing
+
+// type User = {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+// } & UserRoleAttributes
+
+// type UserRoleAttributes = |{
+//   role: "admin";
+//   adminPassword: string;
+// } | { 
+//   role: "user";
+// } | {
+//   role: "super-admin";
+//   superAdminPassword: string;
+// }
+
+
+//Then if we want to key into the role, we would need to do this below. We have index access. In JS, it's UserRoleAttributes.role, but that is invalid in TS. So we use below:
+// type Role = UserRoleAttributes['role']
+
+
+
 
 export const defaultUser: User = {
   id: 1,
